1ec52691eb7bbaff58f021fe502f055aca23b58f
Tuan Nguyen <nguyenvantuan9a7@gmail.com>
2020-10-28 22:40:02 +0700

    push đăng nhập

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('page-login', [TeacherController::class, 'page_login']);
 
 
 Route::group(['prefix' => 'admin'], function () {
