f8efab1c7d5cf2c5d9d85ccd26d4c41c24c59f84
Huynh Net <huynhnet27@gmail.com>
2020-11-01 12:24:36 +0700

    add page student

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,17 @@
 <?php
 use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 //Trang đăng nhập
 Route::get('page-login', [TeacherController::class, 'page_login']);
 
 //Trang chủ giáo viên
 Route::get('page-home-teacher', [TeacherController::class, 'page_home_teacher']);
 
+
+//Trang chủ học sinh
+Route::get('student', [StudentController::class, 'studentHome']);
 
 
 Route::group(['prefix' => 'admin'], function () {
